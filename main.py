a794470ce90fd761a12e99255c7f8105c6fc1b63
HARISH MORE <harishmore1912@gmail.com>
2026-04-08 23:31:50 +0530

    Fixed Linux disk path

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def home():
 def get_metrics():
     cpu = psutil.cpu_percent(interval=1)
     ram = psutil.virtual_memory().percent
-    disk = psutil.disk_usage('C:\\').percent
+    disk = psutil.disk_usage('/').percent
 
     data = {
         "cpu_usage": cpu,
